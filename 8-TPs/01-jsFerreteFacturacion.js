47e6c2142090d095fd68fc197552d948f7371199
Joaquin Mendez <86836925+JoaquinSebastianMendez@users.noreply.github.com>
2021-07-12 19:58:28 -0300

    Add files via upload

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,88 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var NumeroIngresado1;
+	var NumeroIngresado2;
+	var NumeroIngresado3;
+	var mensajeSalida;
+	var Suma;
+
+	NumeroIngresado1 = document.getElementById('txtIdPrecioUno').value;
+	NumeroIngresado1 = parseInt(NumeroIngresado1);
+	
+	NumeroIngresado2 = document.getElementById('txtIdPrecioDos').value;
+	NumeroIngresado2 = parseInt (NumeroIngresado2);
+	
+	NumeroIngresado3 = document.getElementById('txtIdPrecioTres').value;
+	NumeroIngresado3 = parseInt(NumeroIngresado3);
+	
+	Suma = NumeroIngresado1 + NumeroIngresado2 + NumeroIngresado3;
+
+	alert(Suma);	
+
+
 	
 }
 function Promedio () 
 {
+	var NumeroIngresado1;
+	var NumeroIngresado2;
+	var NumeroIngresado3;
+	var Promedio;
+
+	NumeroIngresado1 = document.getElementById('txtIdPrecioUno').value;
+	NumeroIngresado1 = parseInt(NumeroIngresado1);
+	
+	NumeroIngresado2 = document.getElementById('txtIdPrecioDos').value;
+	NumeroIngresado2 = parseInt(NumeroIngresado2);
+	
+	NumeroIngresado3 = document.getElementById('txtIdPrecioTres').value;
+	NumeroIngresado3 = parseInt(NumeroIngresado3);
+
+	Promedio = NumeroIngresado1 % NumeroIngresado2 % NumeroIngresado3;
+
+	alert(Promedio);
+
 	
 }
 function PrecioFinal () 
 {
+	var NumeroIngresado1;
+	var NumeroIngresado2;
+	var NumeroIngresado3;
+	var Suma;
+	var Iva1;
+	var Iva2;
 	
+
+		NumeroIngresado1 = document.getElementById('txtIdPrecioUno').value;
+		NumeroIngresado1 = parseInt(NumeroIngresado1);
+
+		NumeroIngresado2 = document.getElementById('txtIdPrecioDos').value;
+		NumeroIngresado2 = parseInt(NumeroIngresado2);
+
+		NumeroIngresado3 = document.getElementById('txtIdPrecioTres').value;
+		NumeroIngresado3 = parseInt(NumeroIngresado3);
+
+		Suma = NumeroIngresado1 + NumeroIngresado2 + NumeroIngresado3;
+
+		Iva1 = (Suma * 21) / 100;
+
+		Iva2 = Suma + Iva1
+
+		alert(Iva2);
+
+	
+
+
+
+
+
+
+
 }
+
+
+	//txtIdPrecioUno
+	//txtIdPrecioDos
+	//txtIdPrecioTres
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,57 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var LargodelTerreno;
+	var AnchodelTerreno;
+	
+	var Rectangulo;
+
+	LargodelTerreno = document.getElementById('txtIdLargo').value;
+	LargodelTerreno = parseFloat(LargodelTerreno);
+
+	AnchodelTerreno = document.getElementById('txtIdAncho').value;
+	AnchodelTerreno = parseFloat(AnchodelTerreno);
+
+	Rectangulo = ((AnchodelTerreno * 2 ) + (LargodelTerreno * 2))*3;
+
+	alert(" Los metros necesarios para hacer el perimetro del terreno son" ) +Rectangulo;
 
 }
 function Circulo () 
 {
-	
+	var radio;
+	var resultado;
+
+	radio = document.getElementById('txtIdRadio').value;
+	radio = parseFloat(radio);
+
+	resultado = (2 * 3.14 * radio) * 3;
+
+	alert("La cantidad de alambre a comprar es:" + resultado);
 }
+
+
 function Materiales () 
 {
-	
+	var LargodelTerreno;
+	var AnchodelTerreno;
+	var areadelTerreno;
+	var resultadoCemento;
+	var resultadoCal;
+
+	LargodelTerreno = document.getElementById('txtIdLargo').value;
+	LargodelTerreno = parseFloat(LargodelTerreno);
+	AnchodelTerreno = document.getElementById('txtIdAncho').value;
+	AnchodelTerreno = parseInt(AnchodelTerreno);
+	areadelTerreno = AnchodelTerreno * LargodelTerreno;
+
+	resultadoCemento = areadelTerreno * 2;
+	resultadoCal = areadelTerreno * 3;
+
+	alert(resultadoCemento + " Y " + resultadoCal);
 }
+
+
+//txtIdLargo
+//txtIdAncho
+//txtIdRadio
